0030c435d437e94f03b8b1d9974bb5f5ba40f1c1
InterFinB <interbiz2025@gmail.com>
2026-04-07 13:48:30 +0200

    describe what changed

--- hedge-the-edge-web/app/page.tsx
+++ hedge-the-edge-web/app/page.tsx
@@ -192,8 +192,8 @@ export default function Home() {
                     <Image
                       src="/hedge-icon.png"
                       alt="Hedge the Edge"
-                      width={40}
-                      height={40}
+                      width={60}
+                      height={60}
                       className="h-10 w-10 object-cover scale-[1.6]"
                       priority
                     />
